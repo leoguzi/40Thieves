3d755de8595a3abe283428f23eb7987797d2ef6f
Leonardo Guzi <leoguzi@hotmail.com>
2024-09-29 13:56:28 -0300

    Feat: Translate the interface to portuguese

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
-# 40Thiefs
+# PT 
+#  40 Ladrões
+
+Implementação em Python do jogo "40 Ladrões" descrita aqui: https://www.youtube.com/watch?feature=shared&amp;v=ghporyNawck
+
+Instruções para executar o jogo utilizando o powershell do windows:
+
+1. Navegue até a pasta scr utilizando o comando "cd src".
+2. Crie um ambiente virtual utilizando o comando "python -m venv virtualenv".
+3. Ative o ambiente virtual utilizando o comando ".\virtualenv\bin\Activate.ps1". Se você não tiver permissão para a execução de scripts no powershell, atribua a mesma utilizando o comando "Set-ExecutionPolicy RemoteSigned".
+4. Instale as dependencias com o comando "pip install -r ../requirements.txt". Por enquanto esse passo não é necessário, pois o projeto ainda não tem dependências.
+5. Volte ao diretorio pai utilizando "cd ..".
+6. Execute o jogo com o comando "python src/forty_thieves.py".
+
+
+# EN
+# 40 Thieves
+
 Python Implementation of the "40 Ladrões" game described  here: https://www.youtube.com/watch?feature=shared&amp;v=ghporyNawck
 
 Instructions to run the game with windows powershell:
 
-1. Navigate to src  (cd src)
-2. Create a virtual environment (python -m venv virtualenv)
-3. Activate the virtualenv (.\virtualenv\bin\Activate.ps1) (if you don't have the permission to run scripts in powershell set it executing the "Set-ExecutionPolicy RemoteSigned" command.)
-3. Install the requirements (pip install -r ../requirements.txt) (no requirements for this first version, you can ignore this step)
-4. Go back one level (cd..)
-5. Run the game (python src/forty_thiefs.py)
+1. Navigate to src  (cd src).
+2. Create a virtual environment using command "python -m venv virtualenv".
+3. Activate the virtualenv using command ".\virtualenv\bin\Activate.ps1". If you don't have the permission to run scripts in powershell set it executing the "Set-ExecutionPolicy RemoteSigned" command.
+4. Install the requirements using "pip install -r ../requirements.txt". No requirements for this first version, you can ignore this step for now.
+5. Go back one level "cd .."
+6. Run the game "python src/forty_thieves.py".
--- src/player_interface.py
+++ src/player_interface.py
@@ -6,14 +6,14 @@ class PlayerInterface:
     def __init__(self):
       self.main_window = Tk()
       self.fill_main_window() # Fill the main window with the game interface
-      player_name = simpledialog.askstring(title="Player identification", prompt="What is your name?")
-      message = "Welcome to 40 Thiefs, " + player_name + "!"
-      messagebox.showinfo("Welcome!", message, type="ok")
+      player_name = simpledialog.askstring(title="Identificação do jogador", prompt="Como você se chama?")
+      message = "Bem vindo ao 40 Ladrões, " + player_name + "!"
+      messagebox.showinfo("Bem vindo!", message, type="ok")
       self.main_window.mainloop()
       
       
     def fill_main_window(self):  
-      self.main_window.title("Forty Thieves")
+      self.main_window.title("40 Ladrões")
       self.main_window.geometry("620x820")
       self.main_window.resizable(False, False)
       self.main_window["bg"]="azure3"
@@ -54,7 +54,7 @@ class PlayerInterface:
               a_column.append(aLabel)
             self.board_view.append(a_column)
             
-            self.message_label = Label(self.title_frame, bg="azure3", text="40 Thiefs", font="arial 30")
+            self.message_label = Label(self.title_frame, bg="azure3", text="40 Ladrões", font="arial 30")
             self.message_label.grid(row=0, column=0)
             self.turn_label = Label(self.info_frame, bg="azure3", text="Sua vez!", font="arial 24")
             self.turn_label.grid(row=0, column=0)
@@ -66,20 +66,15 @@ class PlayerInterface:
             self.main_window["menu"] = self.menubar
             # add options to the menu bar:
             self.menu_file = Menu(self.menubar)
-            self.menubar.add_cascade(menu=self.menu_file, label="File")
+            self.menubar.add_cascade(menu=self.menu_file, label="Menu")
             # add options to the dropdown menus
-            self.menu_file.add_command(label="Start game", command=self.start_match)
-            self.menu_file.add_command(label="Reset to original state", command=self.start_game)
+            self.menu_file.add_command(label="Iniciar jogo", command=self.start_match)
     
     def select_board_place(self, event, line, column):
-      message = "You selected the position: " + str(line) + ", " + str(column)
+      message = "Você seleconou a posição: " + str(line) + ", " + str(column)
       messagebox.showinfo("Click!", message, type="ok")
       
     def start_match(self):
-      message = "Game initialized!"
-      messagebox.showinfo("Game started!", message, type="ok")
-      
-    def start_game(self):
-      message = "Game reset!"
-      messagebox.showinfo("The game was reseted!", message, type="ok")
+      message = "Uma partida foi iniciada!"
+      messagebox.showinfo("Nova partida", message, type="ok")
        
